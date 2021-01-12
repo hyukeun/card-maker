3eeea5ce5c440cc22516b7a2c66e6353c7b4140f
hyukeun <65886697+hyukeun@users.noreply.github.com>
2021-01-12 22:48:58 +0900

    card-maker : firebase Auth

---
+++ card-maker/app.jsx
@@ -0,0 +1,12 @@
+// import './app.css';
+
+function App() {
+  return (
+    <h1>Hello </h1>
+  );
+}
+
+export default App;
+
+// react component인지 순수 자바스크립트 클래스코드이거나 이런 파일들을 만드는 경우가 있는데  헷갈림을 방지하기 위해 jsx로 설정을 해주었다.
+// index는 엄밀히 말하면 자바스크립트이다. 컴포넌트가 아니며 단순히 연결해주는 역할만 하기 때문이다. 
